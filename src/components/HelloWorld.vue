783782ff9edd3c0e98b1073401ac5f0a19a5592c
untitled2 <somaiaqaiad77@gmail.com>
2022-10-21 10:03:21 -0700

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="hello">
+    <p>For a guide and recipes on how to configure / customize this project,</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container"><HelloWorld /></div>
+</template>
+<script>
+import HelloWorld from "../components/HelloWorld.vue";
+export default {
+  name: "SignUp",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
