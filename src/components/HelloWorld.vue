baf4a2b04386e3fcff2b4d45c6846fefbb0983a5
untitled2 <somaiaqaiad77@gmail.com>
2022-10-21 10:32:34 -0700

    add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,38 @@
 <template>
-  <div class="hello">
-    <p>For a guide and recipes on how to configure / customize this project,</p>
-  </div>
+  <form @click.prevent>
+    <div class="row g-3 align-items-center">
+      <h1>Sign Up</h1>
+      <div class="col-auto d-block mx-auto">
+        <input type="text" class="form-control" placeholder="Enter your Name" />
+      </div>
+    </div>
+    <br />
+    <div class="row g-3 align-items-center">
+      <div class="col-auto d-block mx-auto">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter your Email"
+        />
+      </div>
+    </div>
+    <br />
+    <div class="row g-3 align-items-center">
+      <div class="col-auto d-block mx-auto">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter your Number"
+        />
+      </div>
+    </div>
+    <br />
+    <div class="row g-3 align-items-center">
+      <div class="col-auto d-block mx-auto">
+        <button type="submit" class="btn btn-secondary">Sign</button>
+      </div>
+    </div>
+  </form>
 </template>
 <script>
 export default {
